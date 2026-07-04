ca416931298772022ed2f68910002db98925dd51
Gabriel <gabriel@novavida.com>
2026-07-04 07:58:21 -0300

    feat: adicionados blindados semi

---
+++ resources/[system]/[blindados]/Blindados_Semi/fxmanifest.lua
@@ -0,0 +1,29 @@
+
+shared_script ""
+fx_version 'adamant'
+game 'gta5'
+
+files {
+	"data/**/*",
+	"data/**",
+	"data/*",
+	"data/**/handling.meta",
+	"data/**/vehiclelayouts.meta",
+	"data/**/vehicles.meta",
+	"data/**/carcols.meta",
+	"data/**/carvariations.meta",
+	"audio/*",
+	"audio/**/*",
+}
+-- METAS
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+
+
+
+-----------| Oficial Black Cars |----------
+
+              
